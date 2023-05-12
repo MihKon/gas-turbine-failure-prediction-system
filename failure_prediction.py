b71670625cabf3647b77c7aefa386eaed98375ae
miha- <kondr-mih@mail.ru>
2023-05-12 21:45:43 +0300

    update failure prediction, need model!

--- failure_prediction.py
+++ failure_prediction.py
@@ -1,10 +1,10 @@
 import os
+import glob
 import pandas as pd
+import tensorflow as tf
 from pathlib import Path
-from database import crud, models
-
-MODELS = crud.get_models()
-PARAMS = list(map(lambda x: x.split('_')[0], crud.get_parameters_list())) 
+from database import crud
+from collections import defaultdict
 
 
 def get_current_dataset():
@@ -15,21 +15,36 @@ def get_current_dataset():
     parent = str(path.parent.parent.absolute())
     path_to_file = ''.join([parent, '\\datasets_to_predict'])
 
-    files = os.listdir(path_to_file)
+    files = glob.glob(path_to_file + '\\' + '*.csv')
     if files:
-        lst_files = [os.path.join(path_to_file, file) for file in files]
-        files = [file for file in lst_files if os.path.isfile(file)]
-        dataset_file = max(files, os.path.getctime)
+        files = sorted(files, key=os.path.getctime)
+        dataset_file = files[0]
     
     return dataset_file
 
 
 def predict_params(row, predict_lst, hours):
 
-    for param, val in row.items():
-        predict_model = crud.get_model_by_title(''.join([param, '_{}H'.format(hours)]))
-        param_pred = predict_model.predict(val)
-        predict_lst.update([(param, param_pred)])
+    for param in row.keys():
+
+        model_title = ''.join([param, '_{}.h5'.format(hours)])
+        
+        try:
+            model_path = crud.get_model_by_title(model_title).path_to_file
+            print('Модель найдена!', model_path)
+        except:
+            print('Модель для данного параметра не найдена!', param)
+            continue
+        
+        predict_model = tf.keras.models.load_model(model_path)
+
+        # код не будет работать с этого момента, пока нет модели с нормальным форматом данных
+        try:
+            param_pred = predict_model.predict(row.values())
+            print(param_pred, '\n')
+            predict_lst[param].append(param_pred)
+        except ValueError:
+            print('Неверный формат данных')
     
     return predict_lst
 
@@ -41,8 +56,8 @@ def predict_verdict():
 dataset = get_current_dataset()
 data = pd.read_csv(dataset)
 
-predicts_24h = {}.fromkeys(PARAMS)
-predicts_72h = {}.fromkeys(PARAMS)
+predicts_24h = defaultdict(list)
+predicts_72h = defaultdict(list)
 
 datarows = data.to_dict('records')
 for row in datarows:
